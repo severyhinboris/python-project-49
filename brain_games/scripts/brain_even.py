73b539521961a704e3f811879bfd4123e56f28b2
Boris Severyhin <severyhinboris@gmail.com>
2024-12-11 13:33:40 +0300

    исправлен скрипт brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,11 +1,13 @@
+#!/usr/bin/env python3
+
 from random import randint
+from brain_games.cli import welcome_user
 
 
-def brain_even():   
-    print('Answer "yes" if the number is even, otherwise answer "no".')
+def brain_even(name):
     correct_ans_row = 0
-    while correct_ans_row < 3: 
-        num = randint(1, 1000) 
+    while correct_ans_row < 3:
+        num = randint(1, 1000)
         print('Question:', num)
         ans = input('Your answer: ')
         if num % 2 == 0:
@@ -18,9 +20,19 @@ def brain_even():
             if correct_ans_row == 3:
                 print(f"Congratulations, {name}")
         else:
-            print(f"'{ans}' is wrong answer ;(. Correct answer was 
-'{correct_ans}'.")
+            print(f"""'{ans}' is wrong answer ;(.
+Correct answer was '{correct_ans}'.""")
             print(f"Let's try again, {name}")
             correct_ans_row = 0
             break
-        
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    name = welcome_user()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    brain_even(name)
+
+
+if __name__ == "__main__":
+    main()
